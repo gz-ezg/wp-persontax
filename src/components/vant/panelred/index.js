f46f557ed5624da1e9c36273d8239bea8b251527
roderickt1an <mr_t1an@foxmail.com>
2018-12-30 23:08:04 +0800

    update tax-accout

---
+++ src/components/vant/panelred/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var _component = require('./../common/component.js');
+
+(0, _component.VantComponent)({
+  classes: ['footer-class'],
+  props: {
+    desc: String,
+    title: String,
+    status: String,
+    headerClass: String,
+    useFooterSlot: Boolean
+  }
+});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbImNsYXNzZXMiLCJwcm9wcyIsImRlc2MiLCJTdHJpbmciLCJ0aXRsZSIsInN0YXR1cyIsImhlYWRlckNsYXNzIiwidXNlRm9vdGVyU2xvdCIsIkJvb2xlYW4iXSwibWFwcGluZ3MiOiI7O0FBQUE7O0FBQ0EsOEJBQWM7QUFDWkEsV0FBUyxDQUFDLGNBQUQsQ0FERztBQUVaQyxTQUFPO0FBQ0xDLFVBQU1DLE1BREQ7QUFFTEMsV0FBT0QsTUFGRjtBQUdMRSxZQUFRRixNQUhIO0FBSUxHLGlCQUFhSCxNQUpSO0FBS0xJLG1CQUFlQztBQUxWO0FBRkssQ0FBZCIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IFZhbnRDb21wb25lbnQgfSBmcm9tICcuLi9jb21tb24vY29tcG9uZW50JztcblZhbnRDb21wb25lbnQoe1xuICBjbGFzc2VzOiBbJ2Zvb3Rlci1jbGFzcyddLFxuICBwcm9wczoge1xuICAgIGRlc2M6IFN0cmluZyxcbiAgICB0aXRsZTogU3RyaW5nLFxuICAgIHN0YXR1czogU3RyaW5nLFxuICAgIGhlYWRlckNsYXNzOiBTdHJpbmcsXG4gICAgdXNlRm9vdGVyU2xvdDogQm9vbGVhblxuICB9XG59KTsiXX0=
